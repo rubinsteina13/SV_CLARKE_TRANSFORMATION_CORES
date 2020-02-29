7781a652c93ce2849efbd5d3545cbb86e7df3bd2
Serhii Yatsenko <royalroad1995@gmail.com>
2020-02-29 16:29:12 +0200

    Update forw_clarke_module.sv
    Alignment editing.

--- forw_clarke_module.sv
+++ forw_clarke_module.sv
@@ -6,14 +6,14 @@
 module forw_clarke_module #(
 	parameter DATA_WIDTH = 16
 )(
-	input	logic							clk_i, rst_i,
+	input	logic				clk_i, rst_i,
 	input	logic signed [DATA_WIDTH-1:0]	a_i, b_i,
 	output	logic signed [DATA_WIDTH-1:0]	al_o, be_o
 );
 	const	logic signed [DATA_WIDTH-1:0]	one_div_sqrt3 = 0.577350269 * (2**(DATA_WIDTH-1)-1);
-			logic signed [DATA_WIDTH-1:0]	a_latched, b_latched;
-			logic signed [DATA_WIDTH:0]		a_plus_2b;
-			logic signed [DATA_WIDTH*2-2:0]	be;
+		logic signed [DATA_WIDTH-1:0]	a_latched, b_latched;
+		logic signed [DATA_WIDTH:0]	a_plus_2b;
+		logic signed [DATA_WIDTH*2-2:0]	be;
 
 	always_ff @(posedge clk_i, posedge rst_i) begin
 		if(rst_i) begin
@@ -26,8 +26,8 @@ module forw_clarke_module #(
 	end
 
 	assign	al_o		= a_latched,
-			a_plus_2b	= a_latched + signed'({b_latched, 1'b0}),
-			be			= a_plus_2b * one_div_sqrt3,
-			be_o		= be[DATA_WIDTH*2-2:DATA_WIDTH-1];
+		a_plus_2b	= a_latched + signed'({b_latched, 1'b0}),
+		be		= a_plus_2b * one_div_sqrt3,
+		be_o		= be[DATA_WIDTH*2-2:DATA_WIDTH-1];
 			
 endmodule: forw_clarke_module
